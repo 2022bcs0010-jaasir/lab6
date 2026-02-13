2dcc2acfdc121d53612a860edeed9faf5f46560e
Amogh-4 <jaasir.subair@gmail.com>
2026-02-13 17:04:41 +0530

    rf split=0.25 n_set=80 max_depth=15

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import json
 import pickle
 import pandas as pd
 
-from sklearn.linear_model import Ridge
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -21,18 +21,20 @@ X = df.drop("quality", axis=1)
 y = df["quality"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.20, random_state=42)
+    X, y, test_size=0.25, random_state=42)
 
-model = Ridge(alpha=0.5, fit_intercept=True)
+model = RandomForestRegressor(
+    n_estimators=80, max_depth=15, random_state=42)
+
 model.fit(X_train, y_train)
 
 pred = model.predict(X_test)
 
-mse_exp01 = mean_squared_error(y_test, pred)
-r2_exp01 = r2_score(y_test, pred)
+mse_exp06 = mean_squared_error(y_test, pred)
+r2_exp06 = r2_score(y_test, pred)
+print("RF-01 MSE:", mse_exp06)
+print("RF-01 R2 :", r2_exp06)
 
-print("LR-01 MSE:", mse_exp01)
-print("LR-01 R2 :", r2_exp01)
 
 
 
@@ -44,8 +46,8 @@ with open("output/model/trained_model.pkl", "wb") as f:
     pickle.dump(model, f)
 
 metrics = {
-    "MSE": mse_exp01,
-    "R2": r2_exp01
+    "MSE": mse_exp06,
+    "R2": r2_exp06
 }
 
 with open("app/artifacts/metrics.json", "w") as f:
