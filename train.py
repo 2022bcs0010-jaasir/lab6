e4cf9bcd6f06f5dc27b10858fd68ec37de904991
Amogh-4 <jaasir.subair@gmail.com>
2026-02-13 17:08:50 +0530

    ridge with feature selection alpha=0.5 split=0.20

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import json
 import pickle
 import pandas as pd
 
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -20,20 +20,27 @@ df = pd.read_csv("dataset/winequality-red.csv", sep=";")
 X = df.drop("quality", axis=1)
 y = df["quality"]
 
+corr = X.corrwith(y).abs().sort_values(ascending=False)
+top5_features = corr.index[:5]
+
+print("Selected Features:", list(top5_features))
+
+X_selected = X[top5_features]
+
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=42)
+    X_selected, y, test_size=0.20, random_state=42
+)
 
-model = RandomForestRegressor(
-    n_estimators=80, max_depth=15, random_state=42)
-
+model = Ridge(alpha=0.5, fit_intercept=True)
 model.fit(X_train, y_train)
 
 pred = model.predict(X_test)
+g
+mse_exp01_fs = mean_squared_error(y_test, pred)
+r2_exp01_fs = r2_score(y_test, pred)
 
-mse_exp06 = mean_squared_error(y_test, pred)
-r2_exp06 = r2_score(y_test, pred)
-print("RF-01 MSE:", mse_exp06)
-print("RF-01 R2 :", r2_exp06)
+print("LR-01 (Feature Selected) MSE:", mse_exp01_fs)
+print("LR-01 (Feature Selected) R2 :", r2_exp01_fs)
 
 
 
@@ -46,8 +53,8 @@ with open("output/model/trained_model.pkl", "wb") as f:
     pickle.dump(model, f)
 
 metrics = {
-    "MSE": mse_exp06,
-    "R2": r2_exp06
+    "MSE": mse_exp01_fs,
+    "R2": r2_exp01_fs
 }
 
 with open("app/artifacts/metrics.json", "w") as f:
