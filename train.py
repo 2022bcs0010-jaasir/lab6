4d1c939077759d28458ea8adc3cf31b183c73b33
Amogh-4 <jaasir.subair@gmail.com>
2026-02-13 11:17:07 +0530

    lab6

---
+++ train.py
@@ -0,0 +1,52 @@
+import os
+import json
+import pickle
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+print("Name: Mohamed Jaasir Subair")
+print("Roll no: 2022BCS0010")
+
+os.makedirs("output/model", exist_ok=True)
+os.makedirs("output/results", exist_ok=True)
+
+df = pd.read_csv("dataset/winequality-red.csv", sep=";")
+
+X = df.drop("quality", axis=1)
+y = df["quality"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+model = RandomForestRegressor(
+    n_estimators=100, max_depth=10, random_state=42)
+
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+mse_exp05 = mean_squared_error(y_test, pred)
+r2_exp05 = r2_score(y_test, pred)
+print("RF-01 MSE:", mse_exp05)
+print("RF-01 R2 :", r2_exp05)
+
+
+
+
+
+
+
+with open("output/model/trained_model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+metrics = {
+    "MSE": mse_exp05,
+    "R2": r2_exp05
+}
+
+with open("output/results/metrics.json", "w") as f:
+    json.dump(metrics, f, indent=4)
